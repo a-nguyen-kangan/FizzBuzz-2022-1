0ad8fcc1e8aeb44e7c8f443d2785978b7ea3bd9e
Anh Nguyen <anguyen@nguyen-mbp.kbtm.kangan.edu.au>
2022-10-05 15:43:18 +1100

    1st version complete

--- Program.cs
+++ Program.cs
@@ -25,4 +25,27 @@ while (!int.TryParse(Console.ReadLine(), out buzzNum)) {
 System.Console.WriteLine(maxNum);
 
 // Version 1 - using multiple counters
+var fizzCounter = 1;
+var buzzCounter = 1;
+var counter1 = 1;
 
+while (counter1 <= maxNum) {
+    if (fizzCounter == fizzNum && buzzCounter == buzzNum) {
+        System.Console.WriteLine("FizzBuzz");
+        fizzCounter = 1;
+        buzzCounter = 1;
+    }
+    else if(fizzCounter == fizzNum) {
+        System.Console.WriteLine("Fizz");
+        fizzCounter = 0;
+    } else if (buzzCounter == buzzNum) {
+        System.Console.WriteLine("Buzz");
+        buzzCounter = 0;
+    } else {
+        System.Console.WriteLine(counter1);
+    }
+
+    counter1++;
+    fizzCounter++;
+    buzzCounter++;
+}
